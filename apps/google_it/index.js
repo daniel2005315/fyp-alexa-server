963f55dfce05f153cd98b5ba42f617c092702406
Daniel_CK <daniel2005315@gmail.com>
2017-10-30 21:23:12 +0800

    Initial commit

---
+++ apps/google_it/index.js
@@ -0,0 +1,19 @@
+var alexa = require('alexa-app');
+
+// Allow this module to be reloaded by hotswap when changed
+module.change_code = 1;
+
+// Define an alexa-app
+var app = new alexa.app('google_it');
+app.launch(function(req, res) {
+  res.say("What you want me to do?");
+});
+
+app.intent('GoogleIt', {
+  "slots": { "SearchItem": "LITERAL" },
+  "utterances": ["{Search for|Google|I want to know about} {Alexa|Latest Movies|Weather|Nearest clinic|Jokes}"]
+}, function(req, res) {
+  res.say('Your want to search about ' + req.slot('SearchItem'));
+});
+
+module.exports = app;
