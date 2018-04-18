0ca3a94620e01525374a11644d86428aa1de6876
daniel2005315 <daniel2005315@gmail.com>
2018-04-19 01:04:47 +0800

    fixed syntax error

--- server/app.js
+++ server/app.js
@@ -152,7 +152,7 @@ module.exports = function(express,alexaAppServerObject) {
   // Access token (long lived): rnbw0w2L4LHCCHnRU07CjzH42oYYN7INtOpXoHqsSOJibHfhUpKI7UUN/t8xlZbLh8GqNefkYOtD5iFbvPLvDP3XyKPtmUdZWO2E4JWxhxNmIfSpNbjszL8uneB+eSEEmCmf9Th1KFFhKDSgQWHnKwdB04t89/1O/w1cDnyilFU=
   express.post('/line/webhook', function(req,res){
     console.log("[line.webhook] Triggered");
-    console.log(req.body.);
+    console.log(req.body.events);
     res.sendStatus(200);
   });
 
